e382ede158757e1a45666bc49f214548ea313e21
Rancidwhale <muhammadabdullah3602@gmail.com>
2026-01-10 15:26:09 +0530

    second setup

--- test.py
+++ test.py
@@ -1,35 +1,34 @@
-no_of_servers=int(input("no of servers"))
-load_of_servers=[]
-print("enter server loads")
-for i in range(no_of_servers):
-    q = int(input(str(i)+": "))
-    load_of_servers.append(q)
 
-no_of_req=int(input("no of requests"))
+c = 0
 
-# for _ in range(no_of_req):
-#     min = 9999999999999999
-#     idx=0
-#     for i, load in enumerate(load_of_servers):
-#         if min>load:
-#             min=load
-#             idx=i
-            
-#     load_of_servers[idx]=load_of_servers[idx]+1
+def stepcount(n, ways,curSum):
+    global c 
+    print(ways,curSum,c)
 
+    if curSum==n:
+        c+=1
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+    
+    if n == 2:
+        return 2
+    
+    if curSum+1<=n:
+        ways.append(1)
+        curSum+=1
+        stepcount(n, ways.copy(),curSum)
+    
+    print(ways,curSum,c)
 
-#     print("updated load ", load_of_servers)
+    
 
-load_of_servers.sort()
-for _ in range(no_of_req):
-    i=1
-    while load_of_servers[i]==load_of_servers[i-1]:
-        i+=1
-        if i == no_of_servers:
-            break
-    if i==1:
-        load_of_servers[i-1]+=1
-        # load_of_servers[i], load_of_servers[i+1] = load_of_servers[i+1], load_of_servers[i]
-    else:
-        load_of_servers[i-1]+=1
-    print("updated load ", load_of_servers)
+    if curSum+2<=n:
+        ways.append(2)
+        curSum+=2
+        stepcount(n, ways.copy(),curSum)
+
+    
+
+k = stepcount(3,[],0)
